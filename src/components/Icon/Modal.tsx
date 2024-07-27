a23b515c9ed784d93aba94f004ccc0238dd4bf49
Dheeresh Agarwal <da11@iitbbs.ac.in>
2024-07-27 10:54:18 +0530

    max-w-xl

--- src/components/Icon/Modal.tsx
+++ src/components/Icon/Modal.tsx
@@ -15,7 +15,7 @@ const Modal: React.FC<ModalProps> = ({ icon, onClose }) => {
 
   return (
     <div className="fixed inset-0 backdrop-blur-lg flex justify-center items-center">
-      <div ref={modalRef} className="drop-shadow w-full mx-4 px-2 py-10 rounded-lg shadow-lg relative max-w-2xl bg-gray-200 overflow-auto">
+      <div ref={modalRef} className="drop-shadow w-full mx-4 px-2 py-10 rounded-lg shadow-lg relative max-w-xl bg-gray-200 overflow-auto">
         {icon.classes.map((iconClass, index) => (
           <IconCode key={iconClass} iconClass={iconClass} onClose={onClose} />
         ))}
